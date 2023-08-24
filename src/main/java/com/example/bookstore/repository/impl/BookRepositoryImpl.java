a6315d8baa76bbd2577b8b58a182430dbe0b73b7
-admin- <viktoriiamaherko@gmail.com>
2023-08-24 17:03:49 +0300

    Some imports were optimized

--- src/main/java/com/example/bookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/example/bookstore/repository/impl/BookRepositoryImpl.java
@@ -2,13 +2,12 @@ package com.example.bookstore.repository.impl;
 
 import com.example.bookstore.model.Book;
 import com.example.bookstore.repository.BookRepository;
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository {
--- src/main/java/com/example/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/bookstore/service/impl/BookServiceImpl.java
@@ -3,10 +3,9 @@ package com.example.bookstore.service.impl;
 import com.example.bookstore.model.Book;
 import com.example.bookstore.repository.BookRepository;
 import com.example.bookstore.service.BookService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
