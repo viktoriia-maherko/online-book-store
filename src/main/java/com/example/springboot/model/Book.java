fb9376c199760a2c60d4166ac38184c6d0f8eb73
-admin- <viktoriiamaherko@gmail.com>
2023-09-06 23:07:40 +0300

    Validation for model class was deleted

--- src/main/java/com/example/springboot/model/Book.java
+++ src/main/java/com/example/springboot/model/Book.java
@@ -1,12 +1,11 @@
 package com.example.springboot.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
@@ -21,17 +20,16 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotNull
+    @Column(nullable = false)
     private String title;
-    @NotNull
+    @Column(nullable = false)
     private String author;
-    @NotNull
+    @Column(nullable = false)
     private String isbn;
-    @NotNull
-    @Min(0)
+    @Column(nullable = false)
     private BigDecimal price;
     private String description;
     private String coverImage;
-    @NotNull
+    @Column(nullable = false)
     private boolean isDeleted = false;
 }
