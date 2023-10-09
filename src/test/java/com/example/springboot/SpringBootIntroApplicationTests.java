b025117da7bf580b0e3cb3eeff6fe589ebdb926b
-admin- <viktoriiamaherko@gmail.com>
2023-10-09 21:04:49 +0300

    Added Docker

--- src/test/java/com/example/springboot/SpringBootIntroApplicationTests.java
+++ src/test/java/com/example/springboot/SpringBootIntroApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.springboot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootIntroApplicationTests {
 
-    @Test
-    void contextLoads() {
+    /*@Test
+    //void contextLoads() {
     }
+     */
 
 }
